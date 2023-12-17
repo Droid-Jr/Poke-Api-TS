143e680759ae8124c0c26bd7cf337b7b24176674
joseph <droidjr10@gmail.com>
2023-12-17 01:48:22 -0500

    xd

--- src/services/PokemonIdeApi.tsx
+++ src/services/PokemonIdeApi.tsx
@@ -5,12 +5,14 @@ import { useEffect, useState } from 'react';
 
 export const PokemonIdeApi = (endpoint: string) => {
   const [Datos, setDatos] = useState<Ipokemon | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect( ()  => {
    ( async () => {
     try {
       const { data} = await axios.get(endpoint, { timeout: 5000 }) ;
       setDatos(data)
+      setLoading(false);
     } catch (error) {
       console.error(error);
     }
@@ -18,5 +20,5 @@ export const PokemonIdeApi = (endpoint: string) => {
    
   }, [endpoint]);
 
-  return [Datos];
+  return {Datos, loading};
 };
